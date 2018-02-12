9c98e54d6288c93073ee695febbe57cce5247ae8
YinzhuSU <31676438+YinzhuSU@users.noreply.github.com>
2018-02-11 19:55:53 -0500

    Update twitter_yzsu.py

--- API Exercise1/twitter_yzsu.py
+++ API Exercise1/twitter_yzsu.py
@@ -94,7 +94,7 @@ def get_all_tweets(screen_name):
         # Performs label detection on the image file
         response = client.label_detection(image=image)
         labels = response.label_annotations
-        for i in image:   #
+        #for i in image:   #
         print('Labels:')
         file.write('Lables:')
         for label in labels:
