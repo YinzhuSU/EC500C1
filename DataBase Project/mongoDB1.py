30b5fdd6bcb011bed0f596794f741cdb25ceb440
YinzhuSU <31676438+YinzhuSU@users.noreply.github.com>
2018-04-04 16:36:51 -0400

    upload

---
+++ DataBase Project/mongoDB1.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+import pprint
+import json
+import bson
+
+client = MongoClient()
+db = client.airports.database
+collection = db.airports_collection
+
+posts = db.posts
+
+
+with open('airports.json') as json_file:
+	new_json = json.loads(json_file.read())
+	for info in new_json:
+		posts.insert_one(info)
+
+pprint.pprint(posts.find_one( { 'city': 'Chongqing'}))
